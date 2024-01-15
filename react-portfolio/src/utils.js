98dd1e6ab39355960397417a83518b8f6c7943cf
AyeshaAziz <ayeshaaziz44436@gmail.com>
2024-01-15 21:20:31 +0000

    Updated Utils.js

--- react-portfolio/src/utils.js
+++ react-portfolio/src/utils.js
@@ -1,3 +1,4 @@
+const PATH_PREFIX = '/assets/'; 
 export const getImageUrl = (path) => {
-    return new URL(`/assets/${path}`, import.meta.url).href;
+    return new URL(`${PATH_PREFIX}${path}`, import.meta.url).href;
 };
